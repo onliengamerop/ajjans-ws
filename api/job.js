471f09692ce3b7f7d5be4974c7ceac609ed5cadb
onliengamerop <85559565+onliengamerop@users.noreply.github.com>
2026-04-04 22:50:28 +0530

    Change export syntax from default to module.exports

--- api/job.js
+++ api/job.js
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+module.exports = (req, res) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     
     const servers = [
@@ -15,4 +15,4 @@ export default function handler(req, res) {
     ];
     
     res.status(200).json(servers);
-}
+};
